49e5cc96bece86c69a2beab7a410537c0b5889a0
shawn25n92 <shawn25n92@gmail.com>
2017-10-20 11:57:19 +0800

    JSTableBuilder Initial Commit
    Initial Commit

---
+++ JSTableBuilder/JSTableBuilder.js
@@ -0,0 +1,24 @@
+﻿function UDTable(Tid, Tname, Tclass, Tstyle) {
+    this.Tid = Tid;
+    this.Tname = Tname;
+    this.Tclass = Tclass;
+    this.Tstyle = Tstyle;
+}
+function UDTableHead(THid, THname, THclass, THstyle) {
+    this.THid = THid;
+    this.THname = THname;
+    this.THclass = THclass;
+    this.THstyle = THstyle;
+}
+function UDTableBody(TBid, TBname, TBclass, TBstyle) {
+    this.TBid = TBid;
+    this.TBname = TBname;
+    this.TBclass = TBclass;
+    this.TBstyle = TBstyle;
+}
+function UDTableRow(TRid, TRname, TRclass, TRstyle) {
+    this.TRid = TRid;
+    this.TRname = TRname;
+    this.TRclass = TRclass;
+    this.TRstyle = TRstyle;
+}
