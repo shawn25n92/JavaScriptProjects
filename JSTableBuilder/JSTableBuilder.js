39caaa49ebccfcf19514045bb5e039c4927c332e
shawn25n92 <shawn25n92@gmail.com>
2017-10-20 15:08:21 +0800

    JSTableBuilder new functions
    New UDTableColumn. This is the simplest object in this library
    
    UDTableRow.AddUDTableColumn will add multiple UDTableColumns to the row
    
    UDTableBody and UDTableHead both have UDTableRow collections
    
    Added a primitive render function to UDTable

--- JSTableBuilder/JSTableBuilder.js
+++ JSTableBuilder/JSTableBuilder.js
@@ -3,22 +3,94 @@
     this.Tname = Tname;
     this.Tclass = Tclass;
     this.Tstyle = Tstyle;
+    this.UDTableHeadCollection = [];
+    this.UDTableBodyCollection = [];
+    this.AddUDTableHead = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            this.UDTableHeadCollection.push(arguments[i])
+        }
+    }
+    this.AddUDTableBody = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            this.UDTableBodyCollection.push(arguments[i])
+        }
+    }
+    this.Render = function (display_location_selector, selector_type) {
+        var display_location;
+        // render the table first
+        var table_main = document.createElement("table");
+        table_main.id = this.Tid != null ? this.Tid : "";
+        if (this.Tname != null) {
+            table_main.setAttribute("name", this.Tname);
+        }
+        if (this.Tclass != null) {
+            table_main.setAttribute("class", this.Tclass);
+        }
+        if (this.Tstyle != null) {
+            table_main.setAttribute("style", this.Tstyle);
+        }
+        if (this.UDTableHeadCollection.length >= 1) {
+            var table_head = document.createElement("thead");
+            table_main.appendChild(table_head);
+        }
+
+        if (display_location_selector != null && selector_type != null) {
+            if (selector_type == "id") {
+                display_location = document.getElementById(display_location_selector);
+            }
+            else if (selector_type == "class") {
+                display_location = document.getElementsByClassName(display_location_selector);
+            }
+            else if (selector_type == "name") {
+                display_location = document.getElementsByName(display_location_selector);
+            }
+        }
+        else {
+            // assume that display_location is a DOM node and append the table to the inner html
+            document.body.appendChild(table_main);
+        }
+    }
 }
 function UDTableHead(THid, THname, THclass, THstyle) {
     this.THid = THid;
     this.THname = THname;
     this.THclass = THclass;
     this.THstyle = THstyle;
+    this.UDTableRowCollection = [];
+    this.AddUDTableRow = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            this.UDTableRowCollection.push(arguments[i]);
+        }
+    }
 }
 function UDTableBody(TBid, TBname, TBclass, TBstyle) {
     this.TBid = TBid;
     this.TBname = TBname;
     this.TBclass = TBclass;
     this.TBstyle = TBstyle;
+    this.UDTableRowCollection = [];
+    this.AddUDTableRow = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            this.UDTableRowCollection.push(arguments[i]);
+        }
+    }
 }
 function UDTableRow(TRid, TRname, TRclass, TRstyle) {
     this.TRid = TRid;
     this.TRname = TRname;
     this.TRclass = TRclass;
     this.TRstyle = TRstyle;
+    this.UDTableColumnCollection = [];
+    this.AddUDTableColumn = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            this.UDTableColumnCollection.push(arguments[i]);
+        }
+    }
+}
+function UDTableColumn(TDid, TDname, TDclass, TDstyle, TDcontent) {
+    this.TDid = TDid;
+    this.TDname = TDname;
+    this.TDclass = TDclass;
+    this.TDstyle = TDstyle;
+    this.TDcontent = TDcontent;
 }
